5099ff986b7a89a98ef67212cce23e343181a785
hasrini <hasrini@iu.edu>
2024-07-04 19:50:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Editor } from '@monaco-editor/react';
+import Navbar from './Navbar';
+import Axios from 'axios';
+import spinner from './spinner.gif';
 
 function App() {
+
+  const [userCode, setUserCode] = useState(``);
+
+  const [userLang, setUserLang] = useState("python");
+
+  const [userTheme, setUserTheme] = useState("vs-dark");
+
+  const [fontSize, setFontSize] = useState(20);
+
+  const [userInput, setUserInput] = useState("");
+
+  const [userOutput, setUserOutput] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const options = {
+    fontsize: fontSize
+  }
+
+  function compile() {
+    setLoading(true);
+
+    if(userCode === ``){
+      return;
+    }
+
+
+    Axios.post(`http://localhost:8000/compile`, {
+      code: userCode,
+      language: userLang,
+      input: userInput
+    }).then((res) => {
+      setUserOutput(res.data.output);
+    }).then(() => {
+      setLoading(false);
+    })
+  }
+
+  function clearOutput() {
+    setUserOutput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar 
+        userLang={userLang} setUserLang={setUserLang}
+        userTheme={userTheme} setUserTheme={setUserTheme}
+        fontSize={fontSize} setFontSize={setFontSize}
+      />
+      <div className='main'> 
+        <div className='left-container'>
+          <Editor 
+            options={options}
+            height="calc(100vh - 50px)"
+            width="100%"
+            theme={userTheme}
+            language={userLang}
+            defaultLanguage='python'
+            defaultValue="# Enter your code here"
+            onChange={(value) => {setUserCode(value)}}
+          />
+          <button className='run-btn' type='submit' onClick={() => compile()}>
+            Run
+          </button>
+        </div>
+
+        <div className='right-container'>
+          <h4>
+            Input : 
+          </h4>
+          <div className='input-box'>
+            <textarea className='code-inp' 
+              onChange={(e) => setUserInput(e.target.value)}>
+            </textarea>
+          </div>
+          <h4>
+            Output :
+          </h4>
+          {loading ? (
+              <div className='spinner-box'>
+                <img src={spinner} alt='loading...'/>
+              </div>
+              
+          ) : (
+              <div className='output-box'>
+                <pre> {userOutput} </pre>
+                <button className='clear-btn' onClick={() => {clearOutput() }}>
+                  Clear
+                </button>
+              </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
